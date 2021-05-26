2c57cb5371b194bc49e052a4f62f14ccdf1e1b8e
Chandan Chainani <chainanichan@gmail.com>
2021-05-27 01:41:22 +0530

    disabled db in rails 6

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,31 @@
 require_relative 'boot'
 
-require 'rails/all'
+#=> # require 'rails/all'
+#=> # require 'active_record/railtie'
+#=> # require 'active_storage/engine'
+#=> require 'action_controller/railtie'
+#=> require 'action_view/railtie'
+#=> require 'action_mailer/railtie'
+#=> require 'active_job/railtie'
+#=> require 'action_cable/engine'
+#=> require 'action_mailbox/engine'
+#=> require 'action_text/engine'
+#=> # require 'rails/test_unit/railtie'
+#=> require 'sprockets/railtie'
+require "rails"
+# Pick the frameworks you want:
+require "active_model/railtie"
+require "active_job/railtie"
+# require "active_record/railtie"
+# require "active_storage/engine"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+# require "action_mailbox/engine"
+# require "action_text/engine"
+require "action_view/railtie"
+require "action_cable/engine"
+require "sprockets/railtie"
+require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -10,6 +35,7 @@ module Test67585007
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    # config.frameworks -= [ :active_record ]
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
